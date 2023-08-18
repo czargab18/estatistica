9ec4d4a3bcc5dc5c018d60c4c7ae39bd84af06d8
czargab18 <cesargabrielcode@gmail.com>
2023-08-18 17:24:27 -0300

    footer-nav: tela<833px botão transição

---
+++ ac/globalfooter/1/pt_BR/scripts/globalfooter.js
@@ -0,0 +1,55 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var columns = document.querySelectorAll(".gf-directory-column");
+
+  columns.forEach(function (column) {
+    var button = column.querySelector(
+      ".gf-directory-column-section-title-button"
+    );
+    var list = column.querySelector(".gf-directory-column-section-list");
+
+    button.addEventListener("click", function () {
+      if (list.style.display === "none" || list.style.display === "") {
+        list.style.display = "block";
+        button.setAttribute("aria-expanded", "true");
+      } else {
+        list.style.display = "none";
+        button.setAttribute("aria-expanded", "false");
+      }
+    });
+
+    var links = column.querySelectorAll(".gf-directory-column-section-link");
+    links.forEach(function (link) {
+      link.addEventListener("click", function () {
+        list.style.display = "none";
+        button.setAttribute("aria-expanded", "false");
+      });
+    });
+  });
+});
+
+/* document.addEventListener("DOMContentLoaded", function () {
+  var button = document.querySelector(
+    ".gf-directory-column-section-title-button"
+  );
+  var list = document.getElementById(
+    "footer-directory-column-section-products"
+  );
+
+  button.addEventListener("click", function () {
+    if (list.style.display === "none" || list.style.display === "") {
+      list.style.display = "block";
+      button.setAttribute("aria-expanded", "true");
+    } else {
+      list.style.display = "none";
+      button.setAttribute("aria-expanded", "false");
+    }
+  });
+
+  var links = document.querySelectorAll(".gf-directory-column-section-link");
+  links.forEach(function (link) {
+    link.addEventListener("click", function () {
+      list.style.display = "none";
+      button.setAttribute("aria-expanded", "false");
+    });
+  });
+}); */
