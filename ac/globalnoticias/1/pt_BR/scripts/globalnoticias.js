ce28b314f5df5c0f5eca3ad588d959feb18458b9
czargab18 <cesargabrielcode@gmail.com>
2023-09-28 21:23:35 -0300

    globalnoticias.js

---
+++ ac/globalnoticias/1/pt_BR/scripts/globalnoticias.js
@@ -0,0 +1,45 @@
+// Adicione seu código JavaScript aqui para implementar a funcionalidade de slider
+    const itemContainer = document.querySelector('.item-container');
+    const galleryItems = document.querySelectorAll('.gallery-item');
+    const playButton = document.getElementById('playButton');
+    
+    let currentIndex = 0;
+    let isPlaying = false;
+    let intervalId;
+    
+    function goToSlide(index) {
+      itemContainer.style.transform = `translateX(-${index * 100}%)`;
+      currentIndex = index;
+    }
+    
+    function startSlider() {
+      if (!isPlaying) {
+        isPlaying = true;
+      //   playButton.textContent = 'Pause';
+        
+        intervalId = setInterval(() => {
+          let nextIndex = currentIndex + 1;
+          if (nextIndex >= galleryItems.length) {
+            nextIndex = 0;
+          }
+          goToSlide(nextIndex);
+        }, 3000); // Altere o intervalo conforme necessário (em milissegundos)
+      }
+    }
+    
+    function pauseSlider() {
+      if (isPlaying) {
+        clearInterval(intervalId);
+        isPlaying = false;
+      //   playButton.textContent = 'Play';
+      }
+    }
+    
+    playButton.addEventListener('click', () => {
+      if (isPlaying) {
+        pauseSlider();
+      } else {
+        // Adicione um atraso antes de iniciar o slider novamente
+        setTimeout(startSlider, 1000); // Atraso de 1 segundo (1000 milissegundos)
+      }
+    });
