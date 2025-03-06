b66df9a90f9c57fde4fcf5d88f543b511ca0b49c
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-03-06 19:32:43 -0300

    feature: implementa funs.import_file()
    Função

---
+++ _backend_/scripts/newsroom/python/_test_fun_.py
@@ -0,0 +1,8 @@
+import funs
+
+# _____ TESTE DA FUNÇÃO _____#
+# > > FUNCIONA CORRETAMENTE < < #
+
+print(funs.create_dir(diretorio="./teste"))
+print(funs.verificacao(pergunta="Você está bem?"))
+print(funs.import_file(caminho="_backend_/scripts/newsroom/article/artigo.txt"))
--- _backend_/scripts/newsroom/python/funs.py
+++ _backend_/scripts/newsroom/python/funs.py
@@ -8,11 +8,30 @@ import shutil
 import requests
 from datetime import datetime
 
-# Função final do resultado da junção da função abaixo:
+
+def create_dir(diretorio):
+    """
+    Verifica se um diretório existe e, caso não exista, cria-o.
+    Args:
+      diretorio (str): O caminho do diretório a ser verificado/criado.
+    Returns:
+      bool: Retorna True se o diretório for criado e False se ele já existia.
+    """
+    if not os.path.exists(diretorio):
+        os.makedirs(diretorio)
+        return True
+    else:
+        return False
+
 def verificacao(pergunta:str):
-  # PERGUNTA
+  """
+  Faz uma pergunta ao usuário e verifica a resposta.
+  Args:
+      pergunta (str): A pergunta a ser feita ao usuário.
+  Returns:
+      bool: Retorna True, False ou None.
+  """
   resposta = input(f'{pergunta}? (s/n): ').lower().strip()
-  # VERIFICAR RESPOSTA DA PERGUNTA
   if resposta in ["sim", "s", "yes", "y"]:
         return True
   elif resposta in ["não", "nao", "n", "no", "ñ"]:
@@ -20,5 +39,15 @@ def verificacao(pergunta:str):
   else:
       return None
 
-#_____ TESTE DA FUNÇÃO _____#
-print(verificacao(pergunta="Você está bem?")) ###__ FUNCIONA __###
+def import_file(caminho: str):
+    """
+    Importa um arquivo a partir de um caminho.
+    Args:
+        caminho (str): O caminho do arquivo a ser importado.
+    Returns:
+        str: Retorna o conteúdo do arquivo.
+    """
+    with open(caminho, "r", encoding="utf-8") as arquivo:
+        conteudo = arquivo.read()
+    return conteudo
+
