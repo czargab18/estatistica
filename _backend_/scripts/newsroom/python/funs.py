0e149fe8a36a18a0b713e39798a5470e8950e695
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-03-06 18:56:58 -0300

    feature: adiciona verificacao()
    - Combina as funções `verificar_resposta` e `fazer_pergunta` em uma única função `verificacao`.
    - A função `verificacao` agora faz a pergunta e verifica a resposta em uma única etapa.
    - Remove a necessidade de uma função separada para verificar a resposta.
    - Mantém a funcionalidade de verificar respostas em português e inglês.
    
    Testes:
    - Adiciona um teste simples para verificar a funcionalidade da nova função `verificacao`.

--- _backend_/scripts/newsroom/python/funs.py
+++ _backend_/scripts/newsroom/python/funs.py
@@ -8,3 +8,17 @@ import shutil
 import requests
 from datetime import datetime
 
+# Função final do resultado da junção da função abaixo:
+def verificacao(pergunta:str):
+  # PERGUNTA
+  resposta = input(f'{pergunta}? (s/n): ').lower().strip()
+  # VERIFICAR RESPOSTA DA PERGUNTA
+  if resposta in ["sim", "s", "yes", "y"]:
+        return True
+  elif resposta in ["não", "nao", "n", "no", "ñ"]:
+      return False
+  else:
+      return None
+
+#_____ TESTE DA FUNÇÃO _____#
+print(verificacao(pergunta="Você está bem?")) ###__ FUNCIONA __###
