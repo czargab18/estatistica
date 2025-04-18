7f132a22857751d80885c37bf37a6490d7e5ba26
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-04-18 18:11:24 -0300

    features(fun|py): adiciona core.dir

--- backend/core/dir.py
+++ backend/core/dir.py
@@ -3,6 +3,8 @@ import os
 def createdir(structure: list = None):
     for path in structure:
         dir_path = os.path.dirname(path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(path, "w") as file:
-            pass
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
+        if not os.path.isdir(path):
+            with open(path, "w") as file:
+                pass
