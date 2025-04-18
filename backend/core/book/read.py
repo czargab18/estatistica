01d071fa4fe9dc00644567d7b01b0672b9ee12b6
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-04-17 23:24:55 -0300

    fix: corrigi e docum. read()

--- backend/core/book/read.py
+++ backend/core/book/read.py
@@ -1,27 +1,58 @@
-import mimetypes
 import os
 import re
-import pandas
+
+"""
+ * Este script é destinado a auxiliar na automatização de books quarto.
+ * Ele será usado principalmente para:
+ * - injetar html (navbar e footer padrão do site) nas páginas do book_html.
+ * - excluir pastas de output_libs_dir dos criadas pelos book_html.
+ * - corrigir links no head dos arquivos book_html.
+ * - corrigir links do search.json de cada book_html.
+ * - criar e deletar a lista dos books disponíveis no repositório.
+"""
+
 CAMINHOS = [
-    # SYS_BASE_CAMINHO
-    re.compile(r'^[A-Z]{3}\d{4}$'),  # PATTERN_BOOKS_NAME
-    os.path.normpath("/backend/data/books/books.json"),  # PATH_BOOKS_JSON
+    "_comment_: caminhos para o repositório do projeto",
+    os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../")).replace("\\", "/"),
+    re.compile(r'^[A-Z]{3}\d{4}$'),
+    os.path.normpath("/backend/data/books/books.json"), 
     os.path.normpath("./newshub/build/conteudo/"),
-    os.path.normpath("./books/"),  # Caminho base para listabooks
-    os.path.normpath("./data/books/"),  # Caminho para salvar JSON
-    os.path.normpath("./books/docs/"),  # Origem para corsearchjson
-    os.path.normpath("./books/"),  # Destino para corsearchjson
-    os.path.normpath("./backend/data/books/books.json"),
+    os.path.normpath("./books/"),
+    os.path.normpath("./data/books/"),
     os.path.normpath("./books/build/include/include-in-body"),
 ]
 
 def read(path: str = None, tipefile: list = None):
+    """
+    Lê o conteúdo de um arquivo no caminho especificado.
+
+    Parâmetros:
+    ----------
+    path : str
+        O caminho completo para o arquivo que será lido.
+    tipefile : list, opcional
+        Uma lista de tipos de arquivos suportados (não utilizado atualmente na lógica).
+
+    Retorna:
+    -------
+    str
+        O conteúdo do arquivo como uma string, caso seja um arquivo de texto.
+
+    Exceções:
+    --------
+    FileNotFoundError
+        Se o arquivo especificado no caminho não for encontrado.
+    UnicodeDecodeError
+        Se o arquivo não puder ser decodificado como UTF-8.
+
+    Exemplo:
+    --------
+    >>> content = read("caminho/para/arquivo.txt")
+    >>> print(content)
+    """
     with open(path, "r", encoding="utf-8") as file:
         return file.read()
 
-
 if __name__ == "__main__":
-    # Testando a função read com um arquivo de exemplo
-    arquivo = os.path.normpath("./backend/data/books/teste.qmd")
-    conteudo = read(arquivo)
-    print(conteudo)
+    ...
+    print("Fim da execução do script!")
