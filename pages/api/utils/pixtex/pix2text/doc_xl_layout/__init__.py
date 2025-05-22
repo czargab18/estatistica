29a2dc587f54e41ceb2b8bf20d9fdde693d19db3
cesargabrielphd <87340795+cesargabrielphd@users.noreply.github.com>
2025-05-22 20:46:49 -0300

    cleanup: atualiza o cache de repositorio

--- pages/api/utils/pixtex/pix2text/doc_xl_layout/__init__.py
+++
@@ -1,4 +0,0 @@
-# coding: utf-8
-# This whole directory is adapted from https://github.com/AlibabaResearch/AdvancedLiterateMachinery.
-# Thanks to the authors.
-from .doc_xl_layout_parser import DocXLayoutParser
